7dfe8ab4eb1909d0a2c7909ed3c72ff4ec2596c9
YourtionGuo <yourtion@gmail.com>
2017-07-20 18:24:05 +0800

    Initial Commit

---
+++ FaceSwap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FaceSwap
+//
+//  Created by YourtionGuo on 20/07/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
